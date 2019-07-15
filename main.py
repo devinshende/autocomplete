249a552708572f7c84bdc938f5fb4a758e6837a2
Devin Shende <devin@paratools.com>
2019-07-15 12:37:35 -0700

    verbose example

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('square', 
-		help="display the square of a given number",
-		type=int)
-args = parser.parse_args() # returns data from the options specified
-print(args.square**2)
+parser.add_argument('-v','--verbose', 
+		help="print out more information",
+		action="store_true")
+args = parser.parse_args()
+print('normal amount of printing')
+if args.verbose:
+	print('extra print statements because this is verbose land')
